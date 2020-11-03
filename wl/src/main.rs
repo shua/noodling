fac553a946b95f64de970e128d896c4b7b70303a
shua <itis@isthisa.email>
2020-11-03 01:59:51 -0500

    wl: pointer events

--- wl/src/main.rs
+++ wl/src/main.rs
@@ -2,6 +2,7 @@ use rusttype::{self, point, Scale};
 use wayland_client::protocol::{
     wl_buffer::{self, WlBuffer},
     wl_compositor::{self, WlCompositor},
+    wl_pointer,
     wl_seat::{self, WlSeat},
     wl_shm::{self, WlShm},
     wl_surface::{self, WlSurface},
@@ -13,20 +14,13 @@ use wayland_protocols::xdg_shell::client::{
     xdg_wm_base::{self, XdgWmBase},
 };
 
-#[derive(Debug, PartialEq, Eq)]
-enum SurfaceState {
-    Created,
-    Configured,
-    Committed,
-}
-
 #[derive(Debug, PartialEq)]
 struct Surface {
     wl: Main<WlSurface>,
     xdg: Main<XdgSurface>,
     toplevel: Main<XdgToplevel>,
     buffer_committed: bool,
-    state: SurfaceState,
+    configured: bool,
 }
 
 #[derive(Debug, Default)]
@@ -34,9 +28,14 @@ struct Data {
     compositor: Option<Main<WlCompositor>>,
     seat: Option<Main<WlSeat>>,
     seat_cap: Option<wl_seat::Capability>,
+    pointer: Option<(f64, f64)>,
+    pointer_prev: Option<(f64, f64)>,
+    pointer_frame: bool,
+    button: Option<wl_pointer::ButtonState>,
+    button_prev: Option<wl_pointer::ButtonState>,
     shm: Option<Main<WlShm>>,
     shm_formats: Vec<wl_shm::Format>,
-    buffer: Option<Main<WlBuffer>>,
+    buffer: Option<ShmPixelBuffer>,
     wmbase: Option<Main<XdgWmBase>>,
     surface: Option<Surface>,
     xdg_toplevel: Option<Main<XdgToplevel>>,
@@ -67,12 +66,14 @@ macro_rules! global_filter {
     };
 }
 
+#[derive(Debug)]
 struct Font {
     font: rusttype::Font<'static>,
     scale: Scale,
     offset: rusttype::Point<f32>,
 }
 
+#[derive(Debug)]
 struct Glyphs<'f> {
     glyphs: Vec<rusttype::PositionedGlyph<'f>>,
     width: f32,
@@ -111,7 +112,7 @@ impl Font {
 }
 
 impl<'f> Glyphs<'f> {
-    fn render(self, d: impl Fn(usize, usize, u8)) {
+    fn render(self, mut d: impl FnMut(usize, usize, u8)) {
         let width = self.width.ceil();
         let height = self.height.ceil();
         for g in self.glyphs {
@@ -124,6 +125,60 @@ impl<'f> Glyphs<'f> {
                         d(x as usize, y as usize, v);
                     }
                 })
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ShmPixelBuffer {
+    wl: Main<WlBuffer>,
+    locked: bool,
+    addr: *mut u32,
+    width: usize,
+    height: usize,
+}
+
+impl std::ops::Index<(usize, usize)> for ShmPixelBuffer {
+    type Output = u32;
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        if x >= self.width || y >= self.height {
+            panic!(
+                "index ({}, {}) out of bounds (0..{}, 0..{})",
+                x, y, self.width, self.height
+            );
+        }
+        unsafe {
+            self.addr
+                .offset((x + y * self.width) as isize)
+                .as_ref()
+                .unwrap()
+        }
+    }
+}
+
+impl std::ops::IndexMut<(usize, usize)> for ShmPixelBuffer {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        if x >= self.width || y >= self.height {
+            panic!(
+                "index ({}, {}) out of bounds (0..{}, 0..{})",
+                x, y, self.width, self.height
+            );
+        }
+        unsafe {
+            self.addr
+                .offset((x + y * self.width) as isize)
+                .as_mut()
+                .unwrap()
+        }
+    }
+}
+
+impl ShmPixelBuffer {
+    fn clear(&mut self, c: u32) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                self[(x, y)] = c;
             }
         }
     }
@@ -145,6 +200,24 @@ fn main() {
                     wl_seat::Event::Capabilities{capabilities} => {
                         println!("seat caps {:?}", capabilities);
                         data.seat_cap.replace(capabilities);
+                    }
+                );
+                let pointer = seat.get_pointer();
+                filter2!(pointer, data,
+                    wl_pointer::Event::Enter { surface_x, surface_y, .. } => {
+                        data.pointer.replace((surface_x, surface_y));
+                    },
+                    wl_pointer::Event::Leave { .. } => {
+                        data.pointer.take();
+                    },
+                    wl_pointer::Event::Motion { surface_x, surface_y, .. } => {
+                        data.pointer.replace((surface_x, surface_y));
+                    },
+                    wl_pointer::Event::Button { button: 0x110, state, .. } => {
+                        data.button.replace(state);
+                    },
+                    wl_pointer::Event::Frame => {
+                        data.pointer_frame = true;
                     }
                 );
                 data.seat.replace(seat);
@@ -214,25 +287,36 @@ fn main() {
                 }
                 data as *mut u32
             };
-            println!("fill shm data ({:?})", shmdata);
-            // 4 bytes in a 32bit word, so size / 4 == size >> 2
-            for i in 0..((size >> 2) as isize) {
-                unsafe { *shmdata.offset(i) = 0xdd222222 }
-            }
-            font.glyphs("привет").render(|x, y, v| {
-                if v > 0x22 {
-                    unsafe {
-                        *shmdata.offset((x + y * (width as usize)) as isize) =
-                            u32::from_be_bytes([0xdd, v, v, v]);
-                    }
-                }
-            });
 
             let pool = shm.create_pool(fd, size);
             let buffer = pool.create_buffer(0, width, height, stride, wl_shm::Format::Argb8888);
             pool.destroy();
 
-            data.buffer.replace(buffer);
+            filter2!(buffer, data,
+                wl_buffer::Event::Release => {
+                    if let Some(buffer) = &mut data.buffer {
+                        buffer.locked = false;
+                    }
+                }
+            );
+
+            println!("fill shm data ({:?})", shmdata);
+            let mut shmbuffer = ShmPixelBuffer {
+                wl: buffer,
+                locked: false,
+                addr: shmdata,
+                width: width as usize,
+                height: height as usize,
+            };
+
+            shmbuffer.clear(0xdd222222);
+            font.glyphs("привет").render(|x, y, v| {
+                if v > 0x22 {
+                    shmbuffer[(x, y)] = u32::from_be_bytes([0xdd, v, v, v]);
+                }
+            });
+
+            data.buffer.replace(shmbuffer);
         }
 
         if let (None, Some(compositor), Some(wmbase)) =
@@ -248,13 +332,13 @@ fn main() {
             filter2!(toplevel, data,
                 xdg_toplevel::Event::Close => data.should_close = true
             );
-            xdg.set_window_geometry(0, 0, 100, 100);
+            xdg.set_window_geometry(0, 0, 480, 320);
             filter2!(xdg, data,
                 xdg_surface::Event::Configure { serial } => {
                     println!("xdg_surface Configure");
                     let surface = data.surface.as_mut().expect("surface configured without surface?");
                     surface.xdg.detach().ack_configure(serial);
-                    surface.state = SurfaceState::Configured;
+                    surface.configured = true;
                 }
             );
             wl.commit();
@@ -263,7 +347,7 @@ fn main() {
                 xdg,
                 toplevel,
                 buffer_committed: false,
-                state: SurfaceState::Created,
+                configured: false,
             });
         }
 
@@ -272,17 +356,64 @@ fn main() {
                 surface
                 @
                 Surface {
-                    state: SurfaceState::Configured,
+                    configured: true,
+                    buffer_committed: false,
                     ..
                 },
             ),
             Some(buffer),
-        ) = (&mut data.surface, &data.buffer)
+        ) = (&mut data.surface, &mut data.buffer)
         {
             println!("buffer commit");
-            surface.wl.attach(Some(buffer), 0, 0);
+            surface.wl.attach(Some(&buffer.wl), 0, 0);
+            buffer.locked = true;
             surface.wl.commit();
-            surface.state = SurfaceState::Committed;
+            surface.buffer_committed = true;
+        }
+
+        if let (true, Some((x, y)), Some(buffer @ ShmPixelBuffer { locked: false, .. })) =
+            (data.pointer_frame, data.pointer, &mut data.buffer)
+        {
+            if match data.pointer_prev {
+                Some((px, py)) => {
+                    let (dx, dy) = (x - px, y - py);
+                    (dx * dx + dy * dy) > 0.0001
+                }
+                None => true,
+            } {
+                println!("redraw (mouse {}, {})", x, y);
+                data.pointer_prev.replace((x, y));
+                buffer.clear(0xdd222222);
+                let x = x.ceil() as usize;
+                let y = y.ceil() as usize;
+                let top = std::cmp::max(0, y as i32 - 10) as usize;
+                let bottom = std::cmp::min(buffer.height, y + 10);
+                let left = std::cmp::max(0, x as i32 - 10) as usize;
+                let right = std::cmp::min(buffer.width, x + 10);
+
+                for i in left..right {
+                    for j in top..bottom {
+                        let dx = x as i32 - i as i32;
+                        let dy = y as i32 - j as i32;
+                        if (dx * dx + dy * dy) < 100 {
+                            buffer[(i, j)] = 0xdd888888;
+                        }
+                    }
+                }
+
+                font.glyphs("привет").render(|x, y, v| {
+                    if v > 0x22 {
+                        buffer[(x, y)] = u32::from_be_bytes([0xdd, v, v, v]);
+                    }
+                });
+
+                if let Some(surface) = &mut data.surface {
+                    surface
+                        .wl
+                        .damage(0, 0, buffer.width as i32, buffer.height as i32);
+                    surface.buffer_committed = false;
+                }
+            }
         }
     }
 }
